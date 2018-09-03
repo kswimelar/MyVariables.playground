b4e64cdb83cf0eb6ce563af6371c401ba41a9108
kswimelar <kswimelar@comcast.net>
2018-09-03 09:10:04 -0500

    Week 2
    Week 2 Assignment

---
+++ week2.swift
@@ -0,0 +1,22 @@
+// Kari Swimelar
+// CIS-280-D010
+// August 30, 2018
+
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Comparison
+
+var intVal1:Int=75
+var intVal2:Int=50
+
+if intVal1 > intVal2 {
+    print("Value is Greater")
+}
+
+
+
+for index in 0...20{
+    print("index = \(index)")
+}
